e9acd0fc72282e74df1f8ede1622e94d0dea3f4b
Pranav Magdum <pranavmagdum148@gmail.com>
2026-08-08 18:05:11 +0530

    Time: 10 ms (10.01%), Space: 11.9 MB (29.74%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+
+        unordered_map<char, int> mp;
+        for(int i = 0; i < magazine.size(); i++) {
+            mp[magazine[i]]++;
+        }
+        for(int i = 0; i < ransomNote.size(); i++) {
+             char ch = ransomNote[i];
+
+             if(mp[ch] == 0) {
+                return false;
+            }
+            mp[ch]--;
+        }
+
+        return true;
+    }
+};
