6e12abd331cf0e6bef84b6c95a9bc16e8602d230
Pranav Magdum <pranavmagdum148@gmail.com>
2026-09-01 19:17:42 +0530

    Time: 1 ms (84.93%), Space: 17.1 MB (56.78%) - LeetHub

---
+++ 0179-largest-number/0179-largest-number.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string largestNumber(vector<int>& nums) {
+        vector<string>arr;
+        for(int i=0;i<nums.size();i++){
+            arr.push_back(to_string(nums[i]));
+        }
+
+        sort(arr.begin(),arr.end(),[](string a,string b){
+            return a+b>b+a;
+        });
+        string ans="";
+        for(auto it:arr){
+            ans+=it;
+        }
+        if(arr[0]=="0"){
+            return "0";
+        }
+        return ans;
+    }
+};
