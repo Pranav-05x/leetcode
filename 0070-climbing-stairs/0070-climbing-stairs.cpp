d446aed91ab870cd0b3e38366dda94d43a17bd26
Pranav Magdum <pranavmagdum148@gmail.com>
2026-09-07 23:47:56 +0530

    Time: 0 ms (100%), Space: 8.7 MB (20.52%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int solve(int n,vector<int>&dp){
+        if(n==0)return 1;
+        if(n==1)return 1;
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+        int left=solve(n-1,dp);
+        int right=solve(n-2,dp);
+        dp[n]=left+right;
+        return dp[n];
+    }
+    int climbStairs(int n) {
+        vector<int>dp(n+1,-1);
+        return solve(n,dp);
+    }
+};
